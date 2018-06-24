10d8f06f3fbef29644212494cc50c11f5224a010
Kartik <kartik2498@outlook.com>
2018-06-25 00:00:26 +0530

    Major changes
    App step detail made scrollable for easy horizontal viewing
    State persisted
    Player state and position persisted

--- app/src/main/java/com/example/kartik/boulangerie/widget/IngredientsWidgetProvider.java
+++ app/src/main/java/com/example/kartik/boulangerie/widget/IngredientsWidgetProvider.java
@@ -1,60 +1,6 @@
 package com.example.kartik.boulangerie.widget;
 
-import android.app.PendingIntent;
-import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.widget.RemoteViews;
 
-import com.example.kartik.boulangerie.R;
-import com.example.kartik.boulangerie.ui.DetailActivity;
-
-import java.util.ArrayList;
-
-import static com.example.kartik.boulangerie.widget.WidgetUpdateService.FROM_ACTIVITY_INGREDIENTS_LIST;
 public class IngredientsWidgetProvider extends AppWidgetProvider {
-
-    public static String REMOTEVIEW_INGREDIENT_LIST="REMOTEVIEW_INGREDIENT_LIST";
-    public static String REMOTEVIEW_BUNDLE="REMOTEVIEW_BUNDLE";
-
-    static ArrayList<String> ingredientsList = new ArrayList<>();
-
-    public static void updateBakingWidgets(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
-    }
-
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {}
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_grid_view);
-
-        Intent intent = new Intent(context, GridWidgetService.class);
-        remoteViews.setRemoteAdapter(R.id.widget_grid_view, intent);
-        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
-    }
-
-    @Override
-    public void onEnabled(Context context) {}
-
-    @Override
-    public void onDisabled(Context context) {}
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, IngredientsWidgetProvider.class));
-        final String action = intent.getAction();
-        if (action.equals("android.appwidget.action.APPWIDGET_UPDATE2")) {
-            ingredientsList = intent.getExtras().getStringArrayList(FROM_ACTIVITY_INGREDIENTS_LIST);
-            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_grid_view);
-            IngredientsWidgetProvider.updateBakingWidgets(context, appWidgetManager, appWidgetIds);
-            super.onReceive(context, intent);
-        }
-    }
 }
-
---
+++ app/src/main/java/com/example/kartik/boulangerie/widget/WidgetRVFactory.java
@@ -0,0 +1,4 @@
+package com.example.kartik.boulangerie.widget;
+
+public class WidgetRVFactory {
+}
